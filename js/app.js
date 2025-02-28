85b0941d36d0fd6f2dfd5ff9a863c4d8a7218b11
davy-alt <davy.nicacio@aluno.senai.br>
2025-02-27 21:24:39 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const page = 4;
+const baseUrl = "https://rickandmortyapi.com/api";
+
+const loadCharacter = async () => {
+  const res = await fetch(`${baseUrl}/character?page=${page}`);
+  const data = await res.json();
+  return { results: data.results.slice(3, 9) };
+};
+
+const loadAllWithPromiseAll = async () => {
+  const character = await loadCharacter();
+  showCharacter(character.results);
+};
+loadAllWithPromiseAll();
+
+const showCharacter = (characters) => {
+  const characterConteiner = document.getElementById("character-conteiner");
+  characters.forEach((character) => {
+    const divCharacter = document.createElement("div");
+    divCharacter.id = `character-${character.id}`;
+    divCharacter.classList.add("character-box");
+
+    divCharacter.innerHTML = `
+      <img id="img-character" src="${character.image}" alt="image character">
+      <article class="character-info">
+        <h3>${character.name}</h3>
+        <span class="${character.status}">${character.status} - ${character.species}</span>
+        <span class="location">Location:</span>
+        <a class="character-link">${character.location.name}</a>
+        <span class="origin">Origin:</span>
+        <a class="character-link">${character.origin.name}</a>
+      </article>
+    `;
+
+    characterConteiner.appendChild(divCharacter);
+
+    divCharacter.addEventListener("click", () => {
+      characterDetails(character.id);
+    });
+  });
+};
+
+const characterDetails = (id) => {
+  const criptId = encryptId(id);
+  window.location.href = `./pages/character.html?id=${criptId}`;
+};
+
+const encryptId = (id) => {
+  return id.toString(36);
+};
---
+++ js/character.js
@@ -0,0 +1,90 @@
+const decryptId = (id) => {
+  return parseInt(id, 36);
+};
+
+// Função para carregar os detalhes do personagem
+const loadCharacter = async (id) => {
+  try {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    if (!res.ok) {
+      throw new Error("Erro ao carregar o personagem");
+    }
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Função para carregar os episódios do personagem
+const loadEpisodes = async (episodeUrls) => {
+  try {
+    const episodes = await Promise.all(
+      episodeUrls.map(async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error("Erro ao carregar os episódios");
+        }
+        return await res.json();
+      })
+    );
+    return episodes;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Função principal que carrega o personagem e os episódios
+const loadAll = async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const idParam = urlParams.get("id");
+
+  if (!idParam) {
+    window.location.href = "../index.html";
+    return;
+  }
+
+  const idDescrypted = decryptId(idParam);
+
+  try {
+    const character = await loadCharacter(idDescrypted);
+    showCharacterDetails(character);
+
+    // Carregar os episódios do personagem
+    const episodes = await loadEpisodes(character.episode);
+    listarepisodios(episodes);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Função para mostrar os detalhes do personagem
+const showCharacterDetails = (character) => {
+  const detailsContainer = document.getElementById("character-card");
+
+  detailsContainer.innerHTML = `
+    <img src="${character.image}" alt="${character.name}" />
+    <h3>${character.name}</h3>
+    <p class="status">
+      <strong>Status:</strong> 
+      <span class="${character.status}"></span> ${character.status}
+    </p>
+    <p><strong>Espécie:</strong> ${character.species}</p>
+    <p><strong>Gênero:</strong> ${character.gender}</p>
+    <p><strong>Origem:</strong> ${character.origin.name}</p>
+    <p><strong>Localização:</strong> ${character.location.name}</p>
+  `;
+};
+
+
+// Função para mostrar a lista de episódios
+const listarepisodios = (episodes) => {
+  const episodeslist = document.getElementById("episodes-list");
+
+  episodes.forEach((episode) => {
+    const li = document.createElement("li");
+    li.textContent = `${episode.name} - ${episode.air_date}`;
+    episodeslist.appendChild(li);
+  });
+};
+
+loadAll();
